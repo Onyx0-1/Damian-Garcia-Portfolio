86a0d86a3f8229903658273983999542751eb55e
Onyx0-1 <damiangg199@gmail.com>
2025-07-10 07:49:07 -0400

    POR FIN

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,8 +11,25 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader()  {
+    const header = document.getElementById ('header');
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+
+}
+
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/
+
+function scrollUp()  {
+    const scrollUp = document.getElementById ('scrollUp');
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+
+}
+
+window.addEventListener('scroll', scrollUp)
 
 /*==================== ABOUT TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]'),
